25825b4257a7ed1f255a1aad81a568498dd961b4
thdgustjd1 <thdgustjd1@naver.com>
2024-01-16 14:36:53 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/campfood/CampfoodApplication.java
@@ -0,0 +1,13 @@
+package com.campfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campfood/CampfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
