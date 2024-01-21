9a33df33015a71dd4f5a0e13225a10929644f715
thdgustjd1 <thdgustjd1@naver.com>
2024-01-21 20:59:24 +0900

    feat : 토큰인터셉터 구현

---
+++ src/main/java/com/campfood/src/member/Auth/JwtTokenInterceptor.java
@@ -0,0 +1,26 @@
+package com.campfood.src.member.Auth;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+public class JwtTokenInterceptor implements HandlerInterceptor{
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
+                             final Object handler) throws ServletException, IOException {
+        final String token = request.getHeader(AuthConstants.AUTH_HEADER_ACCESS);
+        if(token != null) return true;
+        else{
+            request.setAttribute("exception", "AuthenticationException");
+            request.setAttribute("message", "token null");
+            request.getRequestDispatcher("/error").forward(request, response);
+            return false;
+        }
+    }
+}
