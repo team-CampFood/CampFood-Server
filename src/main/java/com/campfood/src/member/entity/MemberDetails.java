8a916afb4c89b3cf3ee2fcf7dba9a0ad5f70bfdf
thdgustjd1 <thdgustjd1@naver.com>
2024-01-21 17:09:31 +0900

    feat : 로그인을 위한 memberDetails 구현

---
+++ src/main/java/com/campfood/src/member/entity/MemberDetails.java
@@ -0,0 +1,53 @@
+package com.campfood.src.member.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Delegate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Getter
+public class MemberDetails implements UserDetails {
+    @Delegate
+    private final Member member;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+/*
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+*/
+    @Override
+    public String getUsername() {
+        return member.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return member.getIsEnable();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return member.getIsEnable();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return member.getIsEnable();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return member.getIsEnable();
+    }
+
+}
