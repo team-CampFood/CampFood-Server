7d98d682c245dce27106efb4335bd7a2259212ad
thdgustjd1 <thdgustjd1@naver.com>
2024-01-21 17:18:53 +0900

    feat : refresh, access token 구분

---
+++ src/main/java/com/campfood/src/member/Auth/AuthConstants.java
@@ -0,0 +1,10 @@
+package com.campfood.src.member.Auth;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AuthConstants {
+    public static final String AUTH_HEADER_REFRESH = "Refresh";
+    public static final String AUTH_HEADER_ACCESS = "Access";
+}
