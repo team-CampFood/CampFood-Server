6b0c1a845252b5efe9b41e610706b6a1a2cd2ee0
thdgustjd1 <thdgustjd1@naver.com>
2024-01-21 17:18:25 +0900

    feat : memberDetailsservice 구현

---
+++ src/main/java/com/campfood/src/member/MemberDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.campfood.src.member;
+
+import com.campfood.common.error.ErrorCode;
+import com.campfood.common.exception.ExampleException;
+import com.campfood.src.member.entity.MemberDetails;
+import com.campfood.src.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class MemberDetailsServiceImpl implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public MemberDetails loadUserByUsername(String loginId) {
+        return memberRepository.findByLoginId(loginId)
+                .map(m -> new MemberDetails(m, Collections.singleton(new SimpleGrantedAuthority(m.getRole().getValue()))))
+                .orElseThrow(() -> new ExampleException("user not exist", ErrorCode.USER_NOT_EXIST));
+    }
+}
