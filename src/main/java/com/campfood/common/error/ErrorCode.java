c8ed947e92eed0f2206c910f2a57925f114c5d8e
thdgustjd1 <thdgustjd1@naver.com>
2024-01-22 13:33:55 +0900

    feat : errorcode 추가

--- src/main/java/com/campfood/common/error/ErrorCode.java
+++ src/main/java/com/campfood/common/error/ErrorCode.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
+    //Auth
     Example(000,"example","example"),
-    MEMBER_NOT_EXIST(500,"code1","user not exist");
+    INPUT_NOT_FOUND(400,"A001", "존재하지 않는 입력입니다."),
+    MEMBER_NOT_EXIST(500,"A002","유저가 존재하지 않습니다."),
+    REFRESH_TOKEN_EXPIRED(401,"AOO3", "리프래시토큰이 만료되었습니다.");
 
     final private int status;
     final private String errorCode;
