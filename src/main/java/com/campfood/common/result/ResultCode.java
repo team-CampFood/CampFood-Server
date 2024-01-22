8dee545f8261ad3be1456367e3a9578b358f8ea7
thdgustjd1 <thdgustjd1@naver.com>
2024-01-22 13:33:01 +0900

    feat : 필요한 resultcode 추가

--- src/main/java/com/campfood/common/result/ResultCode.java
+++ src/main/java/com/campfood/common/result/ResultCode.java
@@ -7,8 +7,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum     ResultCode {
-    Example("e001", "example");
+    Example(000, "e001", "example"),
+    //Auth
+    LOGIN_SUCCESS(200, "A001", "로그인에 성공하였습니다."),
+    SIGNIN_SUCCESS(200,"A002", "회원가입에 성공하였습니다."),
+    NEW_ACCESS_TOKEN_SUCCESS(200,"A003", "엑세스토큰 재발급에 성공하였습니다.");
 
+    private final int status;
     private final String code;
     private final String message;
 }
