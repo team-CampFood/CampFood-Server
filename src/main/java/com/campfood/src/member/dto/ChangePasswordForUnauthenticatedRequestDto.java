104c34bac7f73ac290703fec27f9d8048fdd4255
thdgustjd1 <thdgustjd1@naver.com>
2024-02-07 17:01:05 +0900

    refactor : 비밀번호 패턴 수정

--- src/main/java/com/campfood/src/member/dto/ChangePasswordForUnauthenticatedRequestDto.java
+++ src/main/java/com/campfood/src/member/dto/ChangePasswordForUnauthenticatedRequestDto.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 public class ChangePasswordForUnauthenticatedRequestDto {
     @NotBlank(message = "비밀번호는 필수입력값입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{6,16}", message = "비밀번호는 8~16자 대문자, 소문자, 숫자, 특수문자를 허용합니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{6,16}", message = "비밀번호는 6~16자 대문자, 소문자, 숫자, 특수문자를 허용합니다.")
     private String password;
 
     @NotBlank(message = "이메일은 필수입력값입니다.")
